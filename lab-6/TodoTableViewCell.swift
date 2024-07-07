39f039a3cde30936d91a0ec6b7d95706529ec950
Mayank1225 <mayankmalviya2169@gmail.com>
2024-07-06 20:00:50 -0700

    add images

---
+++ lab-6/TodoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TodoTableViewCell.swift
+//  lab-6
+//
+//  Created by user252704 on 7/6/24.
+//
+
+import UIKit
+
+class TodoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
